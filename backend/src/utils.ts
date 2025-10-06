0e68fe257de5d02135651003ddaba46fddeffb57
novailoveyou <novailoveyoux@gmail.com>
2025-10-06 17:48:06 +0300

    fix: ip

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,15 +1,7 @@
 import type { FastifyRequest } from 'fastify'
+import proxyaddr from '@fastify/proxy-addr'
 
 export const checkIsLimit = (value: number) => value >= 10
 
-export const getClientIP = (request: FastifyRequest) => {
-  const forwarded = request.headers['x-forwarded-for']
-  if (forwarded) {
-    const ips = Array.isArray(forwarded) ? forwarded[0] : forwarded
-    const output = ips?.split(',')[0]?.trim()
-    if (output) output
-  }
-
-  // Fallback to socket remote address
-  return request.socket.remoteAddress || request.ips?.[0] || request.ip
-}
+export const getClientIP = (request: FastifyRequest) =>
+  proxyaddr(request as unknown as any, 'uniquelocal')
